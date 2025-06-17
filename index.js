cae6c84fd02d31cfd65e68e14082b45ef9cfad21
ajeetiquincesoft <50664455+ajeetiquincesoft@users.noreply.github.com>
2025-06-17 11:07:21 +0530

    testing

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 const db = require("./models");
 require("dotenv").config();
-
+const path = require('path');
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,7 +12,8 @@ const userRoutes = require("./routes/route");
 app.use("/api", userRoutes);
 
 // Serve static images
-app.use("/uploads", express.static("images"));
+// app.use("/uploads", express.static("images"));
+app.use('/uploads', express.static(path.join(__dirname, 'images')));
 
 // Use port from .env or fallback to 3000
 const PORT = process.env.PORT || 3000;
