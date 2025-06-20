dd35c18dda28bfb76e426147612a63f9c56db992
ajeetiquincesoft <50664455+ajeetiquincesoft@users.noreply.github.com>
2025-06-20 12:38:47 +0530

    key file ignore

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 // firebase.js
 const admin = require("firebase-admin");
-const serviceAccount = require("./selmahousing-d20b9-firebase-adminsdk-fbsvc-ea9dca1d0e.json");
+const serviceAccount = require("./selmahousing-d20b9-firebase-adminsdk-fbsvc-7c11ae3a46.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
